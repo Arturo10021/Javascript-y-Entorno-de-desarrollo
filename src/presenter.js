a58a444451d81aff617dedc62a8394164b74d42e
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-08-14 23:07:19 -0400

    add multiplicar to submit event last correction

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import sumar from "./sumador";
+import { sumar, multiplicar } from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
