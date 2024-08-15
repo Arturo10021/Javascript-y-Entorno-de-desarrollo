55977ef77d73adafe5b95d147ae1131c7b2e5602
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-08-14 22:56:57 -0400

    add test for multiplicar function

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,4 +4,9 @@ describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
+
+  it("deberia multiplicar dos numeros", () => {
+    expect(multiplicar(5, 7)).toEqual(35);
+  });
 });
+
